f3146ce8681a0b7526d00703677ade584386d2f0
Davy <apple@Apples-MacBook-Pro.local>
2020-01-27 23:17:24 +0800

    enhance footer

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+import "font-awesome/css/font-awesome.min.css"
 import "./src/styles/layout.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./src/styles/global.css"
--- src/components/footer.js
+++ src/components/footer.js
@@ -10,7 +10,20 @@ const Footer = () => {
         height: "100px",
       }}
     >
-      <Col className="text-center">
+      <Col>
+        {" "}
+        <div
+          style={{
+            position: "relative",
+            top: "10px",
+            bottom: "10px",
+            margin: "auto 0",
+          }}
+        >
+          Description
+        </div>
+      </Col>
+      <Col>
         <div
           style={{
             position: "relative",
@@ -20,9 +33,54 @@ const Footer = () => {
           }}
         >
           <footer>
-            Gofo © {new Date().getFullYear()} | Business Enquiry :
-            davy_cheng1221@yahoo.com
+            <div>
+              <i className="fa fa-github"></i> :{" "}
+              <a
+                target="_blank"
+                className="noDecoration"
+                href="https://github.com/Davy21"
+              >
+                Davy21
+              </a>
+            </div>
+            <div>
+              <i className="fa fa-envelope"></i> :{" "}
+              <a className="noDecoration" href="mailto:wei122193@gmail.com">
+                wei122193@gmail.com
+              </a>
+            </div>
+            <div>
+              <i className="fa fa-whatsapp"></i> :{" "}
+              <a
+                target="_blank"
+                className="noDecoration"
+                href="https://api.whatsapp.com/send?phone=60127960821"
+              >
+                012-7960821
+              </a>
+            </div>
           </footer>
+        </div>
+      </Col>
+      <Col>
+        <div
+          style={{
+            position: "relative",
+            top: "10px",
+            bottom: "10px",
+            margin: "auto 0",
+          }}
+        >
+          <div>
+            <i className="fa fa-building"></i> :{" "}
+            <a
+              target="_blank"
+              className="noDecoration"
+              href="https://www.gofo.com.my"
+            >
+              Gofo © {new Date().getFullYear()}
+            </a>
+          </div>
         </div>
       </Col>
     </Row>
