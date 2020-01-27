2421f74d5fca36bdce660d97dea989f8aab618d7
Davy <apple@Apples-MacBook-Pro.local>
2020-01-27 16:00:15 +0800

    - add layout to post.js

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,24 +1,35 @@
 import React from "react"
-import { Image } from "react-bootstrap"
+import Layout from "../components/layout"
+import { Row, Col, Image } from "react-bootstrap"
 
 const Post = ({ location }) => {
   if (location.state) {
     const { image, title, content } = location.state
     return (
-      <div>
-        <div className="text-center">
-          <Image
-            className="thumbnail"
-            src={require(`../images/${image}`)}
-            rounded
-          />
-        </div>
-        <h5>{title}</h5>
-        <p>{content}</p>
-      </div>
+      <Layout>
+        <Row>
+          <Col>
+            <div>
+              <div className="text-center">
+                <Image
+                  className="thumbnail"
+                  src={require(`../images/${image}`)}
+                  rounded
+                />
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h5>{title}</h5>
+            <p>{content}</p>
+          </Col>
+        </Row>
+      </Layout>
     )
   } else {
-    return null
+    return <></>
   }
 }
 
