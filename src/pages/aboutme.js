7a8908be9de027fafa130041a11e3e1f197d3ff7
Davy <apple@Apples-MacBook-Pro.local>
2020-01-27 17:00:41 +0800

    set aboutme with layout

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -1,7 +1,8 @@
 import React from "react"
+import Layout from "../components/layout"
 
 const AboutMe = () => {
-  return <div>Under Construction</div>
+  return <Layout>Under Construction</Layout>
 }
 
 export default AboutMe
